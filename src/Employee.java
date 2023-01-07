8eae93fd1b2b40865a578d9d663d0c8f60c3e74a
Isabelle <isagoubinskaya@gmail.com>
2023-01-07 01:37:03 +0100

    commit

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+public class Employee {
+     String firstName;
+     String lastName;
+     int service;
+     int salary;
+     static int count = 1;
+     int jobID;
+     String name;
+     boolean isFilled;
+
+    public Employee(String firstName, String lastName, int service, int salary, int jobID) {
+        this.salary = salary;
+        this.firstName = firstName;
+        this.jobID = jobID;
+        this.lastName = lastName;
+        this.service = service;
+        isFilled = false;
+        ++count;
+    }
+
+    public int getJobID() {
+        return jobID;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getService() {
+        return service;
+    }
+
+    public void showData() {
+        System.out.println("Employee [jobID = " + jobID + ", salary = " + salary + ", name = " + lastName + ", firstName = " + firstName + ", service = " + service + "]");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Collections;
+import java.util.Scanner; import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int allSalaryCost = 0;
+        Employee[] emp = new Employee[10] ;
+
+        emp[0] = new Employee("Isabelle", "Goubinskaya", 5, 789456, 1);
+        emp[1] = new Employee("Andrei", "Gefer", 4, 1, 2);
+        emp[2] = new Employee("Ouinie", "Lourson", 4, 11212, 3);
+
+        emp[0].showData();
+        emp[1].showData();
+        emp[2].showData();
+
+        allSalaryCost = emp[0].salary + emp[1].salary + emp[2].salary;
+        System.out.println("The global amount of all salary is : " + allSalaryCost);
+
+        int min = 0;
+        int max =0;
+
+        if(emp[0].salary < emp[1].salary && emp[0].salary < emp[2].salary) {
+            min = emp[0].salary;
+        } else if (emp[1].salary < emp[0].salary && emp[1].salary < emp[2].salary) {
+            min = emp[1].salary;
+        }else {
+            min = emp[2].salary;
+        }
+
+        if(emp[0].salary > emp[1].salary && emp[0].salary > emp[2].salary) {
+            max = emp[0].salary;
+        } else if (emp[1].salary > emp[0].salary && emp[1].salary > emp[2].salary) {
+            max = emp[1].salary;
+        }else {
+            min = emp[2].salary;
+        }
+
+        System.out.println("minimum salary is " + min);
+        System.out.println("maximum salary is " + max);
+
+        int averageSalary =0;
+        averageSalary = emp[0].salary + emp[1].salary + emp[2].salary;
+        averageSalary = averageSalary /3;
+        System.out.println("average salary is " + averageSalary);
+
+        System.out.println("fullname of employee " + emp[0].jobID + " " + emp[0].firstName + " " + emp[0].lastName);
+        System.out.println("fullname of employee " + emp[1].jobID + " " + emp[1].firstName + " " + emp[1].lastName);
+        System.out.println("fullname of employee " + emp[2].jobID + " " + emp[2].firstName + " " + emp[2].lastName);
+    }
+}
+
